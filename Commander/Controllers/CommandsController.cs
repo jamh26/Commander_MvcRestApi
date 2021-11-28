b3499d041ed64635ec6a8e1c2a01ba5930206f11
jamh <jamh26@users.noreply.github.com>
2021-11-27 21:44:36 -0700

    feat: add two basic GET endpoints to app
    tested MockCommanderRepo implemention

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    // api/commands
+    //[Route("api/[controller]")]
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var command = _repository.GetCommandById(id);
+
+            return Ok(command);
+        }
+    }
+}
